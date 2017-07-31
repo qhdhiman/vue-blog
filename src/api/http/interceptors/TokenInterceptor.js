c1a9a679c0d6df3d707851e18831f6a60e9da180
邵铁军 <shaotiejun@syswin.com>
2017-07-31 16:03:40 +0800

    完善jwt登录功能

--- src/api/http/interceptors/TokenInterceptor.js
+++ src/api/http/interceptors/TokenInterceptor.js
@@ -3,6 +3,6 @@ import Token from '@/api/Token'
  * token 设置
  */
 export default (request, next) => {
-  request.headers.set('Authorization', Token.getItem())
+  request.headers.set('Authorization', `Bearer ${Token.getItem()}`)
   next()
 }
