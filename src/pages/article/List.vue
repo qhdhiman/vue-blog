49270f24f8176439d9528992f8a3b1fe2a241eb3
邵铁军 <shaotiejun@syswin.com>
2017-08-01 10:46:45 +0800

    增加默一用户知识列表页

--- src/pages/article/List.vue
+++ src/pages/article/List.vue
@@ -5,7 +5,7 @@
     <mu-list>
       <template v-for="item in list">
         <mu-card>
-          <mu-card-header @click="goList(item['user_id']._id)" v-if="item['user_id']" :title="item['user_id'].name" subTitle="欲望以提升热情 毅力以磨平高山">
+          <mu-card-header v-if="item['user_id']" :title="item['user_id'].name" subTitle="欲望以提升热情 毅力以磨平高山">
             <mu-avatar :src="item['user_id'].head" slot="avatar"/>
           </mu-card-header>
           <mu-card-title :title="item.title" :subTitle="item.create_time | time"/>
